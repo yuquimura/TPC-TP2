9ee0373cbb174e7711885e00711dc56810930717
Franco Martin Di Maria <fdimaria@fi.uba.ar>
2021-12-12 10:23:57 -0300

    add: TransactionLog

--- src/transactions/transaction_log.rs
+++ src/transactions/transaction_log.rs
@@ -1,24 +1,71 @@
-// #[cfg(test)]
-// mod tests {
+use crate::{alglobo::transaction_state::TransactionState, services::service_name::ServiceName};
 
-//     #[test]
-//     fn build_should_return_array_with_id_entities_states_and_fees() {
-//         let id = 4000;
-//         let 
-//         let log = TransactionLog::build(
+use super::types::LOG_BYTE;
 
-//         );
+pub struct TransactionLog {
 
-//         let mut expected = vec![LOG_BYTE];
-//         expected.append(&mut id.to_be_bytes().to_vec());
-//         expected.push(AIRLINE_BYTE);
-//         expected.push();
-//         expected.push(HOTEL_BYTE);
-//         expected.push(BANK_BYTE);
+}
 
-//         let expected = vec![
-//             id
-//         ];
-//     }
+impl TransactionLog {
+    #[must_use]
+    pub fn build(
+        id: u64,
+        airline_info: (TransactionState, f64),
+        hotel_info: (TransactionState, f64), 
+        bank_info: (TransactionState, f64)
+    ) -> Vec<u8> {
+        let mut message = vec![LOG_BYTE];
+        message.append(&mut id.to_be_bytes().to_vec());
+        
+        message.push(ServiceName::Airline.byte_code());
+        message.push(airline_info.0.byte_code());
+        message.append(&mut airline_info.1.to_be_bytes().to_vec());
 
-// }
+        message.push(ServiceName::Hotel.byte_code());
+        message.push(hotel_info.0.byte_code());
+        message.append(&mut hotel_info.1.to_be_bytes().to_vec());
+
+        message.push(ServiceName::Bank.byte_code());
+        message.push(bank_info.0.byte_code());
+        message.append(&mut bank_info.1.to_be_bytes().to_vec());
+
+        message
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn build_should_return_array_with_id_entities_states_and_fees() {
+        let id = 4000;
+        let airline_state = TransactionState::Waiting;
+        let airline_fee = 100.0;
+        let hotel_state = TransactionState::Accepted;
+        let hotel_fee = 200.0;
+        let bank_state = TransactionState::Aborted;
+        let bank_fee = 300.0;
+        let log_msg = TransactionLog::build(
+            id,
+            (airline_state, airline_fee),
+            (hotel_state, hotel_fee),
+            (bank_state, bank_fee)
+        );
+
+        let mut expected = vec![LOG_BYTE];
+        expected.append(&mut id.to_be_bytes().to_vec());
+        expected.push(ServiceName::Airline.byte_code());
+        expected.push(airline_state.byte_code());
+        expected.append(&mut airline_fee.to_be_bytes().to_vec());
+        expected.push(ServiceName::Hotel.byte_code());
+        expected.push(hotel_state.byte_code());
+        expected.append(&mut hotel_fee.to_be_bytes().to_vec());
+        expected.push(ServiceName::Bank.byte_code());
+        expected.push(bank_state.byte_code());
+        expected.append(&mut bank_fee.to_be_bytes().to_vec());
+
+        assert_eq!(log_msg, expected);
+    }
+
+}
